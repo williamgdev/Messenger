bff119db6ac399d1e53e044decacc6ae773540f2
yktoledo <yktoledo@hotmail.com>
2016-12-30 21:07:36 -0500

    create Util class with 3 functions, checkIsPhone, requestPermition and getContacts

--- app/src/main/java/com/adnroid/devcuba/messengerproject/MainActivity.java
+++ app/src/main/java/com/adnroid/devcuba/messengerproject/MainActivity.java
@@ -1,16 +1,79 @@
 package com.adnroid.devcuba.messengerproject;
 
+
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.adnroid.devcuba.messengerproject.Utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("dsfsdf", "onCreate: ");
-        //kakakaka
+
+
+        if(Utils.checkIsPhone(this))
+        {
+            init();
+        }else{
+           // showError();
+        }
+
+
+
     }
+
+    private void init() {
+        Utils.requestContactPermition(this);
+    }
+
+
+    //----------------------
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Utils.getContacts(this);
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+
+
+    //--------------------------
+
+
+
+
 }
---
+++ app/src/main/java/com/adnroid/devcuba/messengerproject/Utils/Utils.java
@@ -0,0 +1,103 @@
+package com.adnroid.devcuba.messengerproject.Utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by User on 12/30/2016.
+ */
+
+public class Utils {
+
+
+
+
+
+    private static final String TAG = Utils.class.getSimpleName();
+
+    public static void getContacts(Context context) {
+         List<String> listPhone = new ArrayList<>();
+         List<String> listName = new ArrayList<>();
+
+        Cursor cursorPhone = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null, null, null);
+        Cursor cursorID = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null, null, null);
+        cursorPhone.moveToFirst();
+
+        cursorID.moveToFirst();
+        do{
+            listPhone.add(cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+            Log.d(TAG, "retrieveContactNumber: "+cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+        }while (cursorPhone.moveToNext());
+        cursorPhone.close();
+        do{
+            listName.add(cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+            Log.d(TAG, "retrieveContactNumber: "+cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+        }while (cursorID.moveToNext());
+        cursorID.close();
+        Log.d(TAG, "SIZE: "+listName.size()+" - "+listPhone.size());
+
+    }
+
+
+
+    public static void requestContactPermition(Activity activity){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }else {
+            //ejecutalo aki
+
+            getContacts(activity);
+        }
+
+
+    }
+    public static boolean checkIsPhone(Context context){
+        TelephonyManager manager = (TelephonyManager)context.getSystemService(context.TELEPHONY_SERVICE);
+        if(manager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE){
+            Log.d(TAG, "checkIsPhone: Tablet");
+            return false;
+        }else{
+            Log.d(TAG, "checkIsPhone: Phone");
+            return true;
+        }
+    }
+}
